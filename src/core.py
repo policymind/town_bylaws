61df143dc54bf9799a0ccee9f1530a8bae68df41
aapeebles <aapeebles@gmail.com>
2024-11-15 23:56:25 -0500

    updating main mongo table with flags for ecode and pdf links

--- src/core.py
+++ src/core.py
@@ -1,7 +1,16 @@
 import json
 import datetime
 import boto3
+from progressbar import progressbar
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pymongo import UpdateOne
+from bson.objectid import ObjectId
+import keyring
+
 import src.scrape as sc
+import src.mongo as mg
 
 
 WEBSITE_URL = "https://www.mass.gov/info-details/massachusetts-city-and-town-ordinances-and-by-laws"
@@ -37,3 +46,34 @@ object_key = f'ma_town_list/{file_name}'
 
 # Upload the JSON data to S3
 response = s3.upload_file(file_name, BUCKET_NAME, object_key)
+
+# Create a new client and connect to the server
+client = MongoClient(keyring.get_password('mongodb', 'cluster0'), server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# set db as mydatabase
+mydb = client["mydatabase"]
+# create new table matownpilicy
+towntab = mydb['matownpolicy']
+
+# create ecode var
+towntab.update_many( "bylaws_reg":{"$regex":"ecode360"}}, { "$set": { "ecode" : "True" } }) 
+
+# find towns with clear pdf in url
+pdf_query = {"ecode": {"$ne" :"True"}}
+mydoc2 = towntab.find(pdf_query).to_list()
+
+confirmed_websites = [{'id':str(row.get('_id')), 'has_pdf': mg.confirm_pdf(row.get('bylaws_reg'))} for row in progressbar(mydoc2)]
+filtered_data = [d['id'] for d in confirmed_websites if d['has_pdf'] != 0]
+
+# update mongo table
+updates = []
+for id  in filtered_data:
+    updates.append(UpdateOne({'_id': ObjectId(id)}, {'$set': {'ispdf': 1}}))
+towntab.bulk_write(updates)
