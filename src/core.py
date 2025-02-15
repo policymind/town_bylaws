6b7404f7635866ba6d11e2333b18af8ab49cfe4f
aapeebles <aapeebles@gmail.com>
2025-02-15 14:29:24 -0500

    reorg dirs

--- src/core.py
+++ src/core.py
@@ -39,6 +39,8 @@ with open(file_name, "w") as outfile:
     outfile.write(json_data)
 
 
+
+
 # Create an S3 client
 s3 = boto3.client('s3')
 BUCKET_NAME = 'matownbylaws'
@@ -63,7 +65,7 @@ mydb = client["mydatabase"]
 towntab = mydb['matownpolicy']
 
 # create ecode var
-towntab.update_many( "bylaws_reg":{"$regex":"ecode360"}}, { "$set": { "ecode" : "True" } }) 
+towntab.update_many("bylaws_reg":{"$regex":"ecode360"}, { "$set": { "ecode" : "True" } })
 
 # find towns with clear pdf in url
 pdf_query = {"ecode": {"$ne" :"True"}}
@@ -77,3 +79,64 @@ updates = []
 for id  in filtered_data:
     updates.append(UpdateOne({'_id': ObjectId(id)}, {'$set': {'ispdf': 1}}))
 towntab.bulk_write(updates)
+
+############################################
+
+import urllib.request
+
+url_sample = "https://www.townofchesterfieldma.com/sites/g/files/vyhlif7606/f/uploads/general_town_by-laws_2022.pdf"
+mg.confirm_pdf(url_sample)
+
+download_path = f"downloads/policy_file.pdf"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+
+
+
+req = urllib.request.Request(url_sample, headers=HEADERS)
+with urllib.request.urlopen(req) as response, open(download_path, "wb") as f:
+    f.write(response.read())
+
+import json
+from pathlib import Path
+from docling.document_converter import DocumentConverter
+import pandas as pd
+converter = DocumentConverter()
+
+
+result = converter.convert(download_path)
+
+result_dict = result.document.export_to_dict()
+print(json.dumps(result_dict, indent=2))
+output_dir = Path("")
+
+doc_filename = result.input.file.stem
+print(f"Document filename: {doc_filename}")
+# Iterate over tables in the document and save them as CSV and HTML formats.
+for table_idx, table in enumerate(result.document.tables):
+  table_df: pd.DataFrame = table.export_to_dataframe()
+  print(f"$$ Table {table_idx}")
+  # Save as CSV
+  table_df.to_csv(f"{doc_filename}-table-{table_idx}.csv")
+  # Save as HTML
+  html_filename = output_dir / f"{doc_filename}-table-{table_idx+1}.html"
+  with html_filename.open("w") as fp:
+    fp.write(table.export_to_html())
+
+result_dict.keys()
+result_dict['body']['children'][25]
+
+json_data = json.dumps(result_dict, indent=2)
+
+file_name = f"policy_json.json"
+with open(file_name, "w") as outfile:
+    outfile.write(json_data)
+
+
+import pymupdf4llm
+download_path = f"downloads/policy_file.pdf"
+doc = pymupdf4llm.to_markdown(download_path)
+from pathlib import Path
+Path("output.md").write_bytes(doc.encode())
+print(doc)
